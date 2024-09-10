1c61ef52674326c47992dd7c9bd298d48afa24d7
keh38 <Ken_Hancock@meei.harvard.edu>
2024-09-10 16:54:51 -0400

    added namespace to UDPMulticastServer

--- Packages/com.epl.klib/Runtime/KLib/Network/KLib.Network.UDPMulticastServer.cs
+++ Packages/com.epl.klib/Runtime/KLib/Network/KLib.Network.UDPMulticastServer.cs
@@ -6,112 +6,115 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-public class UDPMulticastServer : MonoBehaviour
+namespace KLib.Network
 {
-    Thread _readThread;
-
-    UdpClient _udp = null;
-    string _name = "";
-    int _port = -1;
-
-    void Start()
+    public class UDPMulticastServer : MonoBehaviour
     {
-    }
+        Thread _readThread;
 
-    public bool IsReceiving { get { return _readThread.IsAlive; } }
+        UdpClient _udp = null;
+        string _name = "";
+        int _port = -1;
 
-    public void StartReceiving(string name, int port)
-    {
-        _name = name;
-        _port = port;
-
-        // create thread for reading UDP messages
-        _readThread = new Thread(new ThreadStart(ReceiveData));
-        _readThread.IsBackground = true;
-
-        _readThread.Start();
-    }
-
-    public void StopReceiving()
-    {
-        Debug.Log("Stopping multicast thread");
-        StopThread();
-    }
-
-    // Stop reading UDP messages
-    private void StopThread()
-    {
-        Debug.Log("IsAlive = " + _readThread.IsAlive.ToString());
-        _readThread.Abort();
-        if (_udp != null)
+        void Start()
         {
-            _udp.Close();
-            Debug.Log("Multicast closed");
         }
 
-        //if (_readThread.IsAlive)
-        //{
-        //    _readThread.Abort();
-        //    _udp.Close();
-        //    Debug.Log("Multicast thread aborted");
-        //}
-    }
+        public bool IsReceiving { get { return _readThread.IsAlive; } }
 
-    private void OnDestroy()
-    {
-        //StopThread();
-    }
-
-    private void ReceiveData()
-    {
-        //        var addy = SRITCP.FindServerAddress();
-        string addy = "";
-        Debug.Log("Multicast listening on: " + addy);
-
-        IPAddress localAddress;
-        if (addy.Equals("localhost"))
+        public void StartReceiving(string name, int port)
         {
-            localAddress = IPAddress.Loopback;
-        }
-        else
-        {
-            localAddress = IPAddress.Parse(addy);
+            _name = name;
+            _port = port;
+
+            // create thread for reading UDP messages
+            _readThread = new Thread(new ThreadStart(ReceiveData));
+            _readThread.IsBackground = true;
+
+            _readThread.Start();
         }
 
-        var ipLocal = new IPEndPoint(localAddress, 10000);
-
-        var address = IPAddress.Parse("234.5.6.7");
-        var ipEndPoint = new IPEndPoint(address, 10000);
-
-        _udp = new UdpClient();
-        _udp.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-        _udp.Client.Bind(ipLocal);
-        _udp.Client.ReceiveTimeout = 500;
-
-        _udp.JoinMulticastGroup(address, localAddress);
-
-        var anyIP = new IPEndPoint(IPAddress.Any, 0);
-
-
-        while (true)
+        public void StopReceiving()
         {
-            try
+            Debug.Log("Stopping multicast thread");
+            StopThread();
+        }
+
+        // Stop reading UDP messages
+        private void StopThread()
+        {
+            Debug.Log("IsAlive = " + _readThread.IsAlive.ToString());
+            _readThread.Abort();
+            if (_udp != null)
             {
-                // receive bytes
-                var bytes = _udp.Receive(ref anyIP);
-                var response = Encoding.Default.GetString(bytes);
-                Debug.Log("Multicast received: " + response);
-
-                if (response.Equals(_name))
-                {
-                    bytes = Encoding.UTF8.GetBytes(_port.ToString());
-                    _udp.Send(bytes, bytes.Length, anyIP);
-                    //Debug.Log("response sent");
-                }
+                _udp.Close();
+                Debug.Log("Multicast closed");
             }
-            catch (Exception ex)
+
+            //if (_readThread.IsAlive)
+            //{
+            //    _readThread.Abort();
+            //    _udp.Close();
+            //    Debug.Log("Multicast thread aborted");
+            //}
+        }
+
+        private void OnDestroy()
+        {
+            //StopThread();
+        }
+
+        private void ReceiveData()
+        {
+            //        var addy = SRITCP.FindServerAddress();
+            string addy = "";
+            Debug.Log("Multicast listening on: " + addy);
+
+            IPAddress localAddress;
+            if (addy.Equals("localhost"))
             {
-                //Debug.Log(ex.Message);
+                localAddress = IPAddress.Loopback;
+            }
+            else
+            {
+                localAddress = IPAddress.Parse(addy);
+            }
+
+            var ipLocal = new IPEndPoint(localAddress, 10000);
+
+            var address = IPAddress.Parse("234.5.6.7");
+            var ipEndPoint = new IPEndPoint(address, 10000);
+
+            _udp = new UdpClient();
+            _udp.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            _udp.Client.Bind(ipLocal);
+            _udp.Client.ReceiveTimeout = 500;
+
+            _udp.JoinMulticastGroup(address, localAddress);
+
+            var anyIP = new IPEndPoint(IPAddress.Any, 0);
+
+
+            while (true)
+            {
+                try
+                {
+                    // receive bytes
+                    var bytes = _udp.Receive(ref anyIP);
+                    var response = Encoding.Default.GetString(bytes);
+                    Debug.Log("Multicast received: " + response);
+
+                    if (response.Equals(_name))
+                    {
+                        bytes = Encoding.UTF8.GetBytes(_port.ToString());
+                        _udp.Send(bytes, bytes.Length, anyIP);
+                        //Debug.Log("response sent");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Debug.Log(ex.Message);
+                }
             }
         }
     }
