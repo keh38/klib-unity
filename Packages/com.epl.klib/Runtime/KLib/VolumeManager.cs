6055063dcce30da7ff1d009dbf7c96e78e0c4df7
Ken Hancock <Ken_Hancock@meei.harvard.edu>
2024-11-19 22:40:54 -0500

    added VolumeManager to KLib namespace

--- Packages/com.epl.klib/Runtime/KLib/VolumeManager.cs
+++ Packages/com.epl.klib/Runtime/KLib/VolumeManager.cs
@@ -1,81 +1,83 @@
 ﻿using System.Runtime.InteropServices;
 using UnityEngine;
 
-public class VolumeManager
+namespace KLib
 {
-    //The Unit to use when getting and setting the volume
-    public enum VolumeUnit
+    public class VolumeManager
     {
-        //Perform volume action in decibels</param>
-        Decibel,
-        //Perform volume action in scalar
-        Scalar
-    }
-
-    public VolumeManager()
-    {
-        Initialize();
-    }
-
-    [DllImport("SystemVolumePlugin")]
-    public static extern float GetVolume();
-
-    [DllImport("SystemVolumePlugin")]
-    public static extern float GetVolumeDB();
-
-    [DllImport("SystemVolumePlugin")]
-    public static extern int SetVolume(float volume);
-
-    [DllImport("SystemVolumePlugin")]
-    public static extern int SetVolumeDB(float volume);
-
-    [DllImport("SystemVolumePlugin")]
-    public static extern int InitializeVolume();
-
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate void LogDelegate(string str);
-
-    [DllImport("SystemVolumePlugin")]
-    public static extern void SetLoggingCallback(System.IntPtr func);
-
-    [DllImport("SystemVolumePlugin")]
-    [return: MarshalAs(UnmanagedType.BStr)]
-    public static extern string GetFriendlyName();
-
-    public float GetMasterVolume(VolumeUnit vUnit)
-    {
-        if (vUnit == VolumeUnit.Scalar)
+        //The Unit to use when getting and setting the volume
+        public enum VolumeUnit
         {
-            return GetVolume();
-        }
-        return GetVolumeDB();
-//        return GetSystemVolume(vUnit);
-    }
-    public void SetMasterVolume(float volume, VolumeUnit vUnit)
-    {
-        Debug.Log("MASTER VOLUME = " + volume + " " + vUnit.ToString());
-
-        if (vUnit == VolumeUnit.Scalar)
-        {
-            SetVolume(volume);
-        }
-        else
-        {
-            SetVolumeDB(volume);
+            //Perform volume action in decibels</param>
+            Decibel,
+            //Perform volume action in scalar
+            Scalar
         }
 
-        //        SetSystemVolume(volume, vUnit);
-    }
-    public string FriendlyName { get { return GetFriendlyName(); } }
+        public VolumeManager()
+        {
+            Initialize();
+        }
 
-    public void PrintLogString(string str)
-    {
-        Debug.Log(str);
-    }
+        [DllImport("SystemVolumePlugin")]
+        public static extern float GetVolume();
 
-    private void Initialize()
-    {
-        InitializeVolume();
-    }
+        [DllImport("SystemVolumePlugin")]
+        public static extern float GetVolumeDB();
 
+        [DllImport("SystemVolumePlugin")]
+        public static extern int SetVolume(float volume);
+
+        [DllImport("SystemVolumePlugin")]
+        public static extern int SetVolumeDB(float volume);
+
+        [DllImport("SystemVolumePlugin")]
+        public static extern int InitializeVolume();
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void LogDelegate(string str);
+
+        [DllImport("SystemVolumePlugin")]
+        public static extern void SetLoggingCallback(System.IntPtr func);
+
+        [DllImport("SystemVolumePlugin")]
+        [return: MarshalAs(UnmanagedType.BStr)]
+        public static extern string GetFriendlyName();
+
+        public float GetMasterVolume(VolumeUnit vUnit)
+        {
+            if (vUnit == VolumeUnit.Scalar)
+            {
+                return GetVolume();
+            }
+            return GetVolumeDB();
+            //        return GetSystemVolume(vUnit);
+        }
+        public void SetMasterVolume(float volume, VolumeUnit vUnit)
+        {
+            Debug.Log("MASTER VOLUME = " + volume + " " + vUnit.ToString());
+
+            if (vUnit == VolumeUnit.Scalar)
+            {
+                SetVolume(volume);
+            }
+            else
+            {
+                SetVolumeDB(volume);
+            }
+
+            //        SetSystemVolume(volume, vUnit);
+        }
+        public string FriendlyName { get { return GetFriendlyName(); } }
+
+        public void PrintLogString(string str)
+        {
+            Debug.Log(str);
+        }
+
+        private void Initialize()
+        {
+            InitializeVolume();
+        }
+    }
 }
